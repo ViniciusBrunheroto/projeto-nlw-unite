65904e317d213d54de3db64c27cb07a776294433
Vinícius Brunheroto <vinibrunheroto12@gmail.com>
2024-05-25 20:57:18 -0300

    Fix config

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,8 +9,8 @@ export function Header() {
        <img src={nlwUniteIcon} />
 
        <nav className="flex items-center gap-5">
-        <NavLink href="/eventos">Eventos</NavLink>
-        <NavLink href="/participantes">Participantes</NavLink>
+        <NavLink href="/projeto-nlw-unite/eventos">Eventos</NavLink>
+        <NavLink href="/projeto-nlw-unite/participantes">Participantes</NavLink>
        </nav>
 
     </div>
